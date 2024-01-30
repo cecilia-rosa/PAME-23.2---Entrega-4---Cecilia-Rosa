4fd45600bb41a8bea769387d3a35b80ebcb488d0
Cecília Rosa <cecilianrosa.20231@poli.ufrj.br>
2024-01-30 17:22:21 -0300

    class validator

--- src/funcionario/dto/create-funcionario.dto.ts
+++ src/funcionario/dto/create-funcionario.dto.ts
@@ -1,9 +1,25 @@
+import { IsEmail, IsNumberString } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+
 export class CreateFuncionarioDto {
+
     nome: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNumberString()
     cpf: number;
+
+    @IsNumberString()
     telefone: number;
+
+    @IsNumberString()
     vendas: number;
-    data_contrato: Date;
+
+    @CreateDateColumn({ name: 'data_cadastro' })
+    data_cadastro: Date;
+
     senha: string;
 }
