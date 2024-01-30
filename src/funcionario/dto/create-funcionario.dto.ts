dbcac8e0b44b2db3df1fa248f4116c0c7bfa646d
Cecília Rosa <cecilianrosa.20231@poli.ufrj.br>
2024-01-30 17:29:18 -0300

    comentarios

--- src/funcionario/dto/create-funcionario.dto.ts
+++ src/funcionario/dto/create-funcionario.dto.ts
@@ -1,24 +1,24 @@
-import { IsEmail, IsNumberString } from "class-validator";
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { IsEmail, IsNumberString } from "class-validator"; //para garantir formatos
+import { CreateDateColumn } from 'typeorm'; //para automatizar a data
 
 
 export class CreateFuncionarioDto {
 
     nome: string;
 
-    @IsEmail()
+    @IsEmail()//confere se vai receber um email
     email: string;
 
-    @IsNumberString()
+    @IsNumberString() //confere se vai receber um numero em string
     cpf: number;
 
-    @IsNumberString()
+    @IsNumberString() //confere se vai receber um numero em string
     telefone: number;
 
-    @IsNumberString()
+    @IsNumberString() //confere se vai receber um numero em string
     vendas: number;
 
-    @CreateDateColumn({ name: 'data_cadastro' })
+    @CreateDateColumn({ name: 'data_cadastro' }) //data automaticamente a do dia da criacao do usuario
     data_cadastro: Date;
 
     senha: string;
