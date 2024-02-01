98b4cd71129cd1315a57e85eebeae9e689588756
Cecília Rosa <cecilianrosa.20231@poli.ufrj.br>
2024-02-01 10:15:44 -0300

    class-validator na main

--- src/funcionario/dto/create-funcionario.dto.ts
+++ src/funcionario/dto/create-funcionario.dto.ts
@@ -1,24 +1,30 @@
-import { IsEmail, IsNumberString } from "class-validator"; //para garantir formatos
-import { CreateDateColumn } from 'typeorm'; //para automatizar a data
+import { IsEmail, IsNotEmpty, IsNumber, IsNumberString, IsString } from "class-validator"; //para garantir formatos
 
 
 
 export class CreateFuncionarioDto {
 
+    @IsNotEmpty()//confere se campo nao ta vazio
+    @IsString() //confere se vai receber string
     nome: string;
 
+    @IsNotEmpty()
     @IsEmail()//confere se vai receber um email
     email: string;
 
+    @IsNotEmpty()
     @IsNumberString() //confere se eh string de numero, string para zero a esquerda nao ser excluido
     cpf: string;
 
+    @IsNotEmpty()
+    @IsNumber() //confere se vai receber um numero
     telefone: number;
 
+    @IsNotEmpty()
+    @IsNumber()
     vendas: number;
 
-    @CreateDateColumn({ name: 'data_cadastro' }) //data automaticamente a do dia da criacao do usuario
-    data_cadastro: Date;
-
+    @IsNotEmpty()
+    @IsString()
     senha: string;
 }
